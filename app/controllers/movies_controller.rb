909987d2851be43192254f0e47ef75b1b9322e50
Jake Levy <jake.r.levy@gmail.com>
2013-02-15 20:32:06 -0800

    changed movie model to return all possible ratings with a class method and added check boxes into the index view

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,6 +7,9 @@ class MoviesController < ApplicationController
   end
 
   def index
+    @all_ratings = Movie.all_ratings
+
+
     if params[:sort].nil?
     @movies = Movie.all
     elsif params[:sort].to_s == "title" or params[:sort].to_s  == "release_date"
